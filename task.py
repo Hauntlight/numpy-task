bfeaf598de2aa50e5dd481af40ba9a60ddd67016
Hauntlight <pc1998@live.it>
2026-05-29 11:34:01 +0200

    Task 2 completata, verificata con run

--- task.py
+++ task.py
@@ -8,11 +8,12 @@ def prodotto_scalare(v1: list, v2: list) -> float:
     b=np.array(v2)
     c = a @ b
     print(c)
-    pass
+    return c
 
 def rango_matrice(m: list) -> int:
     """Sub-task 2: Calcola il rango di una matrice."""
-    pass
+    rank=np.linalg.matrix_rank(m, tol=None, hermitian=False)
+    return rank
 
 def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
     """Sub-task 3: Risolvere un Sistema Lineare."""
@@ -29,7 +30,7 @@ def operazioni_elemento_per_elemento(v1: list) -> tuple:
 
 def main():
     print("Sub-task 1:", prodotto_scalare([1, 2, 3], [4, 5, 6]))
-    print("Sub-task 1:", rango_matrice([[1, 2], [3, 4]]))
+    print("Sub-task 2:", rango_matrice([[1, 2], [3, 4]]))
     print("Sub-task 3:", risolvi_sistema_lineare([[2, 1], [1, 3]], [5, 7]))
     print("Sub-task 4:", correlazione_matrici([[1, 2], [3, 4]], [[2, 4], [6, 8]]))
     print("Sub-task 5:", operazioni_elemento_per_elemento([0, 0.5, 1, -0.5]))
