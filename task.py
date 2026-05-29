68c8f12bb5403e6bbed66c26ebd24850a2d2ac69
Hauntlight <pc1998@live.it>
2026-05-29 11:53:50 +0200

    Task 5 completata, verificata con run

--- task.py
+++ task.py
@@ -31,7 +31,12 @@ def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
 
 def operazioni_elemento_per_elemento(v1: list) -> tuple:
     """Sub-task 5: Restituisce (seno, coseno, arcoseno, arcocoseno) elemento per elemento calcolati sul primo array."""
-    pass
+    v1_array=np.array(v1)
+    array_sin=np.sin(v1_array)
+    array_cos=np.cos(v1_array)
+    array_arcsin=np.arcsin(v1_array)
+    array_arccos=np.arccos(v1_array)
+    return (array_sin, array_cos, array_arcsin, array_arccos)
 
 
 def main():
@@ -39,7 +44,7 @@ def main():
     print("Sub-task 2:", rango_matrice([[1, 2], [3, 4]]))
     print("Sub-task 3:", risolvi_sistema_lineare([[2, 1], [1, 3]], [5, 7]))
     print("Sub-task 4:", correlazione_matrici([[1, 2], [3, 4]], [[2, 4], [6, 8]]))
-    print("Sub-task 5:", operazioni_elemento_per_elemento([0, 0.5, 1, -0.5]))
+    print(f"Sub-task 5:{ operazioni_elemento_per_elemento([0, 0.5, 1, -0.5])}")
 
 if __name__ == "__main__":
     main()
